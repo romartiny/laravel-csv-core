f1579359c65716836791e2998bc797a52c55eebb
Romartiny <romortiny@gmail.com>
2024-02-25 03:49:46 +0300

    creation of the main service for csv and implementation of validation methods

---
+++ app/Services/CsvService.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Interfaces\CsvProductRepositoryInterface;
+use App\Services\Interfaces\CsvResponseServiceInterface as CsvResponseInterface;
+use App\Services\Interfaces\CsvValidateConditionServiceInterface as CsvValidateConditionInterface;
+use App\Services\Interfaces\CsvValidatorServiceInterface as CsvValidatorInterface;
+use App\Services\Interfaces\CsvServiceInterface;
+use App\Iterators\CsvIterator;
+use App\Models\Product;
+use Exception;
+
+class CsvService implements CsvServiceInterface
+{
+    private array $goodCsvRows = [];
+    private array $skippedCsvRows = [];
+    private int $goodRowsCount = 0;
+    private int $skippedRowsCount = 0;
+    private array $headers;
+
+    /**
+     * Constructor for the CsvService class.
+     *
+     * @param CsvProductRepositoryInterface $_csvProductRepository          The CSV product repository
+     * @param CsvResponseInterface $_csvResponse                            The CSV response service
+     * @param CsvValidatorInterface $_csvValidator                          The CSV validator service
+     * @param CsvValidateConditionInterface $_csvValidateConditions         The CSV validation condition service
+     * @param Product $_product                                             The product model
+     */
+    public function __construct(
+        private readonly CsvProductRepositoryInterface        $_csvProductRepository,
+        private readonly CsvResponseInterface                 $_csvResponse,
+        private readonly CsvValidatorInterface                $_csvValidator,
+        private readonly CsvValidateConditionInterface        $_csvValidateConditions,
+        private readonly Product                              $_product
+    ) {}
+
+    /**
+     * Checks if a row of data is valid based on provided headers and data.
+     *
+     * @param array $headers  The headers of the CSV file
+     * @param array $data     The data of a single row
+     * @return bool           Returns true if the row is valid
+     */
+    private function isValidRow(array $headers, array $data): bool
+    {
+        if ($headers === $data) {
+            return true;
+        }
+
+        return $this->_csvValidateConditions->checkCsvConditions($data)
+            && $this->_csvValidator->csvValidateRow($headers, $data);
+    }
+
+    /**
+     * Renders the CSV response with CSV rows
+     *
+     * @return void
+     */
+    private function renderCsvResponse(): void
+    {
+        $this->_csvResponse->renderCsvTable($this->goodCsvRows, true, $this->headers);
+        $this->_csvResponse->renderCsvTable($this->skippedCsvRows, false, $this->headers);
+        $this->_csvResponse->renderCsvResult($this->goodRowsCount, $this->skippedRowsCount);
+    }
+
+    /**
+     *  Parses a single row and appends it to the target array.
+     *
+     * @param array $row            The row of data to be parsed
+     * @param array $targetArray    The array to which the parsed row will be appended
+     * @param int $counter          The counter to be incremented
+     * @return void
+     */
+    private function parseRow(array $row, array &$targetArray, int &$counter): void
+    {
+        $targetArray[] = $row;
+        $counter++;
+    }
+
+    /**
+     * Checks a CSV row for validity and processes it accordingly.
+     *
+     * @param array $currentRow     The CSV row to be checked
+     * @return void
+     */
+    private function checkCsvRows(array $currentRow): void
+    {
+        if ($currentRow === $this->headers) {
+            return;
+        }
+
+        if ($this->isValidRow($this->headers, $currentRow)) {
+            $this->parseRow($currentRow, $this->goodCsvRows, $this->goodRowsCount);
+        } else {
+            $this->parseRow($currentRow, $this->skippedCsvRows, $this->skippedRowsCount);
+        }
+    }
+
+    /**
+     *  Saves the valid CSV rows to the product repository.
+     *
+     * @return void
+     */
+    private function saveValidCsvRows(): void
+    {
+        foreach ($this->goodCsvRows as $goodRow) {
+            $this->_csvProductRepository->createProductRow($goodRow);
+        }
+    }
+
+    /**
+     * Stores CSV data from the given file path.
+     *
+     * @param string $csvPath       The file path of the CSV data
+     * @param string $testOption    The test option flag
+     * @return void
+     * @throws Exception            If an error occurs
+     */
+    public function storeCsvData(string $csvPath, string $testOption): void
+    {
+        try {
+            $csvIterator = new CsvIterator($csvPath, $this->_product->getFillable());
+
+            $this->headers = $csvIterator->headers;
+
+            foreach ($csvIterator as $row) {
+                $this->checkCsvRows($row);
+            }
+
+            if (!$testOption) {
+                $this->saveValidCsvRows();
+            }
+
+            $this->renderCsvResponse();
+        } catch (Exception $e) {
+            $this->_csvResponse->csvErrorResponse($e->getMessage());
+        }
+    }
+}
